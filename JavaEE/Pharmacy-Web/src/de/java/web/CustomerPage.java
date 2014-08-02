87825154066dd940a81ca77f0b03d32895b48c06
Marius550 <m_pilg01@uni-muenster.de>
2014-08-02 11:16:53 +0200

    New Extensions 12
    - Added: Prescription Bill to customer details view
    - To Do: Add e-mail adress to send prescription Bill

--- JavaEE/Pharmacy-Web/src/de/java/web/CustomerPage.java
+++ JavaEE/Pharmacy-Web/src/de/java/web/CustomerPage.java
@@ -1,6 +1,9 @@
 package de.java.web;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
@@ -8,6 +11,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import de.java.domain.customer.Customer;
+import de.java.domain.prescription.Prescription;
 import de.java.ejb.CustomerService;
 import de.java.web.util.Util;
 
@@ -19,9 +23,13 @@ public class CustomerPage implements Serializable {
 
   @EJB
   private CustomerService customerService;
+  
+  private CustomerList customerList;
 
   private long id;
   private Customer customer;
+  
+  private Map<Long, Prescription> customerPrescriptions;
 
   public long getId() {
     return id;
@@ -62,5 +70,48 @@ public class CustomerPage implements Serializable {
   public void setCustomer(Customer customer) {
     this.customer = customer;
   }
+  
+  /**
+   * Convert Collection <Prescription> into HashMap and assign id to each map element
+ * @param collection
+ * @return Map<Long, Prescription> map
+ */
+private Map<Long, Prescription> convertPharmacyCustomerPrescriptionsCollectionIntoHashMap(Collection<Prescription> collection){
+	  Map<Long, Prescription> map = new HashMap<Long, Prescription>();
+	  for (Prescription mPrescription : collection){
+		  map.put(mPrescription.getId(), mPrescription);
+	  }
+	  return map;
+	}
+
+/** Call the CustomerWithPrescriptions collection for a specific customer
+ * @param customer
+ * @return Map<Long, Prescription> based on the CustomerWithPrescriptions collection
+ */
+public Map<Long, Prescription> getAllCustomerPrescriptionsToHashMap(Customer customer){
+	  if (customerPrescriptions == null){
+		  customerPrescriptions = convertPharmacyCustomerPrescriptionsCollectionIntoHashMap(customerService.getCustomerWithPrescriptions(customer.getId()).getPrescriptions());
+	  }
+	  return customerPrescriptions;
+}
+
+/** Iterate over the CustomerWithPrescriptions HashMap and only sum up the total price of prescriptions of the specific customer
+ * @param customer
+ * @return Summed up totalPrice
+ */
+public double getCustomerPrescriptionBill(Customer customer){
+	double totalPrice = 0;
+	  for (Prescription p : customerService.getCustomerWithPrescriptions(customer.getId()).getPrescriptions()){
+		  if (getAllCustomerPrescriptionsToHashMap(customer).get(p.getId()) != null) {
+		  totalPrice = totalPrice + getAllCustomerPrescriptionsToHashMap(customer).get(p.getId()).getTotalPrice();  
+		  }
+	  }
+	  return totalPrice;
+	}
+
+public double getCustomerPrescriptionSum(long id) {
+	  Customer customer = customerService.getCustomer(id);
+	  return getCustomerPrescriptionBill(customer);
+	}
 
 }
